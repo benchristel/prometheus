5f7f07e0c5558e522eff098581bc3c29863a1177
Ben Christel <ben.christel@gmail.com>
2025-10-19 13:42:54 -0700

    Run one test

--- dev/test.js
+++ dev/test.js
@@ -1,6 +1,31 @@
 #!/usr/bin/env node
-import {getAllTests, runTests, formatTestResultsAsText} from "@benchristel/taste"
 
-runTests(getAllTests())
-    .then(formatTestResultsAsText)
-    .then(console.log)
+import {glob} from "node:fs/promises"
+import {join} from "node:path"
+import {
+    getAllTests,
+    runTests,
+    formatTestResultsAsText,
+    reportsFailure,
+} from "@benchristel/taste"
+
+main()
+
+async function main() {
+    const root = [import.meta.dirname, ".."]
+    const testFilePatterns = [
+        join(...root, "src", "**", "*.test.js"),
+    ]
+
+    for await (const path of glob(testFilePatterns)) {
+        await import(path)
+    }
+
+    const results = await runTests(getAllTests())
+        .then(formatTestResultsAsText)
+
+    console.log(results)
+    if (reportsFailure(results)) {
+        process.exit(1)
+    }
+}
